393b7f290a0d013cc4ea099c816f539887d0a241
gbascans <gbascansc@gmail.com>
2024-12-27 16:13:23 -0300

    git ignore

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hola Mundo");
+const express = require("express");
+
+const app = express();
+
+// Routing
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo");
+});
+
+app.listen(4000, () => {
+  console.log("Servidor Funcionando...");
+});
