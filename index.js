2756c5603640b35d84c94207cb699a3a38a6a0cb
gbascans <gbascansc@gmail.com>
2024-12-29 00:28:05 -0300

    nodemon

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require("express");
+//const express = require("express");
+import express from "express"; //cambiamos a notacion ESM
 
 const app = express();
 
 // Routing
-
 app.get("/", (req, res) => {
   res.send("Hola Mundo");
 });
 
-app.listen(4000, () => {
-  console.log("Servidor Funcionando...");
+app.get("/ecommerce", (req, res) => {
+  res.send("Hola ecommerce");
+});
+
+app.get("/blog", (req, res) => {
+  res.send("Hola blog");
+});
+
+app.get("/nosotros", (req, res) => {
+  res.send("Hola Nosotros");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Servidor Funcionando en el puerto:", port);
 });
