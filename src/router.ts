ca497d30a89cc79202176e6159c0731dd010a361
gbascans <gbascansc@gmail.com>
2024-12-29 18:23:04 -0300

    instalamos mongoose

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,14 @@ import { Router } from "express";
 
 const router = Router();
 
+/** Autenticación y Registro */
+router.post("/auth/register", (req, res) => {
+  console.log(req.body);
+  let { name, surname, age } = req.body;
+  let newUser = { name, surname, age };
+  res.send(newUser);
+});
+
 // Routing
 router.get("/", (req, res) => {
   res.send("Hola Mundo en Express / Typescript");
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express"; //cambiamos a notacion ESM
 import router from "./router";
 const app = express();
 
+//Leer datos de formularios
+app.use(express.json());
+
 app.use("/api", router);
 
 export default app;
