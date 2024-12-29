f0a06fac59b8b224ded35f1ae9421323e7938b30
gbascans <gbascansc@gmail.com>
2024-12-29 18:56:52 -0300

    colors

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
+import colors from "colors";
 
 export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(process.env.MONGO_URI);
     const url = `${connection.host}:${connection.port}`;
 
-    console.log(`MongoDB Conectado en ${url}`);
+    console.log(colors.blue(`MongoDB Conectado en ${url}`));
   } catch (error) {
-    console.log(error.message);
+    console.log(colors.bgRed.white.bold(error.message));
     process.exit(1);
   }
 };
