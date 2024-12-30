b945506454124bc1e7669653d79ee5039c4c020d
gbascans <gbascansc@gmail.com>
2024-12-30 12:08:27 -0300

    Agregamos Slug y Handle

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,16 +1,32 @@
 import { Request, Response } from "express";
+import slug from "slug";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    res.status(400).json({ error: "El usuario ya está registrado" });
+  const { handle, name, email, password } = req.body;
+  //comprobar email
+  const emailExist = await User.findOne({ email });
+  if (emailExist) {
+    const error = new Error("El correo se encuentra registrado");
+    res.status(409).json({ error: error.message });
+    return;
+  }
+  //comprobar handle
+  const newHandle = slug(handle, "");
+  const handleExist = await User.findOne({ handle: newHandle });
+  if (handleExist) {
+    const error = new Error("El usuario se encuentra registrado");
+    res.status(409).json({ error: error.message });
     return;
   }
   const hashedPassword = await hashPassword(password);
-  const user = new User({ name, email, password: hashedPassword });
+  const user = new User({
+    handle: newHandle,
+    name,
+    email,
+    password: hashedPassword,
+  });
   try {
     await user.save();
     res.json("Usuario Agregado Correctamente");
