41428fdafe059e3ff59bf1121a0ccbb11afd920a
gbascans <gbascansc@gmail.com>
2025-01-24 00:13:02 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,16 @@ import express from "express"; //cambiamos a notacion ESM
 import "dotenv/config";
 import router from "./router";
 import { connectDB } from "./config/db";
-const app = express();
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 
 connectDB();
+
+const app = express();
+
+//Cors
+app.use(cors(corsConfig));
+
 //Leer datos de formularios
 app.use(express.json());
 
